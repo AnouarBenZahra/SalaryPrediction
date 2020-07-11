2da2f4ddd31c7644cf58e0f9bcf2a1a1adcaa560
Anouar BEN ZAHRA <33717910+AnouarBenZahra@users.noreply.github.com>
2020-07-12 00:19:01 +0200

    first commit

---
+++ SalaryPrediction/SalaryData.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalaryPrediction
+{
+    public class SalaryData
+    {
+        [LoadColumn(0)]
+        public float YearsExperience { get; set; }
+        [LoadColumn(1)]
+        public float Salary { get; set; }
+    }
+}
+ 
---
+++ SalaryPrediction/SalaryPrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalaryPrediction
+{
+    public class SalaryPrediction
+    {
+        [ColumnName("Score")]
+        public float PredictedSalary { get; set; }
+    }
+}
